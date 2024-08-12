4ecc4d07dd6b56faf15a051625990761aa6be373
Varun9548 <varunkhandelwal505050@gmail.com>
2024-08-12 11:36:33 +0530

    forth commit

---
+++ File1.js
@@ -0,0 +1,58 @@
+document.write("hii we are still learning </br>" )
+// document.write("addition=",x+y,"<br>")
+// document.write("subtraction=",x-y,"<br>")
+// document.write("multiplication=",x*y,"<br>")
+// document.write("division=",x/y,"<br>")
+// document.write("DONE")
+// console.log("great grand masti");
+// var x = 120;
+// {
+    //     var y = 30;
+// }
+// console.log(x,y);
+// let x =12;
+// {
+//     let y = 30;
+// }
+// console.log(x,y);
+// let x=10
+// let y=5
+// document.write(x,"<br>",y,"<br>")
+// let sum = x+y;
+// document.write(sum,"<br>")
+// let diff = x-y;
+// document.write(diff,"<br>")
+// let prod = x*y;
+// document.write(prod,"<br>")
+// let quot = x/y;
+// document.write(quot,"<br>")
+// let rem = x%y;
+// document.write(rem,"<br>")
+// let isEqual = x==y;
+// document.write(isEqual,"<br>")
+// let isNotEqual = x!=y;
+// document.write(isNotEqual,"<br>")
+// let isGreater = x>y;
+// document.write(isGreater,"<br>")
+// let logicalAnd = x>y && y<10 ;
+// document.write(logicalAnd,"<br>")
+// let pow= x**y
+// document.write(pow,"<br>")
+
+// let fname = "shredder"
+// document.write("hello ", fname,"<br>")
+// let age = 30;
+// document.write("my age =", age,"<br>")
+// let isstudent= true;
+// document.write(`Student - ${isstudent? "Yes": "No"} <br> ` )
+// let mtvalue= null
+// document.write(`About   ${mtvalue || "write something"} <br> `)
+// let uninitialized;
+// document.write(!uninitialized && "write something" )
+let age = prompt("Enter the age")
+if (age >= 18) {
+    document.write("eligible")
+}
+else {
+    document.write("not eligible")
+}
